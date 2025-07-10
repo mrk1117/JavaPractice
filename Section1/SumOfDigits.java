068208182ac4f1c3d1fb5c582865041ce087422c
Robert Bosoiu <r41052@eon.com>
2025-07-10 09:57:56 +0300

    added SumOfDigits.java

--- Section1/SumOfDigits.java
+++ Section1/SumOfDigits.java
@@ -3,5 +3,16 @@ package Section1;
 // Java Program to print the sum of digits of a given integer.
 
 public class SumOfDigits {
-    
+    public static void main(String[] args) {
+        int num = 12345;
+        int sum = 0;
+
+        while (num != 0) {
+            int lastDigit = num % 10;
+            sum += lastDigit;
+            num /= 10;
+        }
+
+        System.out.println("The sum of the digits is: " + sum);
+    }
 }
